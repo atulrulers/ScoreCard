7745394895ce8ecf5da98422f018b4f773d2b3cf
Atul Kumar <atulrulers@gmail.com>
2022-04-04 00:22:47 +0530

    added some javadoc

--- src/main/java/playmatch/PlayMatch.java
+++ src/main/java/playmatch/PlayMatch.java
@@ -5,19 +5,25 @@ import teamstat.TeamStat;
 
 public class PlayMatch {
     private final BasicMatchSetup   basicMatchSetup   = new BasicMatchSetup();
-    private       BattingAndBowling battingAndBowling = new BattingAndBowling();
-    final         TeamStat          teamStat          = new TeamStat();
+    private final BattingAndBowling battingAndBowling = new BattingAndBowling();
+    private final TeamStat          teamStat          = new TeamStat();
 
+    /**
+     * Control will call this method to play the game
+     * 1. It does pre match setup
+     * - Creates team
+     * - decide which team will play first
+     * 2. Each inning of game is played
+     * 3. Shows stat after inning
+     */
     public void playCricket() {
         final MatchSetup matchSetup = basicMatchSetup.beforeMatchSetup();
         battingAndBowling.battingAndBowlingFirst(matchSetup.getBattingTeam(), matchSetup.getBowlingTeam(),
                 matchSetup.getNumberOfOver());
         teamStat.showBattingTeamStatInMatch(matchSetup.getBattingTeam());
-        //        teamStat.showBattingTeamStatInMatch(matchSetup.getBowlingTeam());
         System.out.printf("Playing second inning %n");
         battingAndBowling.battingAndBowlingSecond(matchSetup.getBowlingTeam(), matchSetup.getBattingTeam(),
                 matchSetup.getNumberOfOver());
         teamStat.showBattingTeamStatInMatch(matchSetup.getBowlingTeam());
-        //        teamStat.showBattingTeamStatInMatch(matchSetup.getBattingTeam());
     }
 }
